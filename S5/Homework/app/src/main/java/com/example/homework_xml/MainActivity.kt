96855ed895b08597eabba6434fcc0c90bc9f6493
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2025-04-24 00:28:15 -0600

    Validations

--- S5/Homework/app/src/main/java/com/example/homework_xml/MainActivity.kt
+++ S5/Homework/app/src/main/java/com/example/homework_xml/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.homework_xml
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.homework_xml.databinding.ActivityMainBinding
@@ -18,22 +19,36 @@ class MainActivity : AppCompatActivity() {
 
     private fun inicializar() {
         binding.inclCapitalContable.btnCalcularCapital.setOnClickListener() {
-            val activos = binding.inclCapitalContable.etActivosTotales.text.toString().toDouble()
-            val pasivos = binding.inclCapitalContable.etPasivosTotales.text.toString().toDouble()
+            val activos = binding.inclCapitalContable.etActivosTotales.text.toString().toDoubleOrNull()
+            val pasivos = binding.inclCapitalContable.etPasivosTotales.text.toString().toDoubleOrNull()
+            if (activos == null || activos < 0 || pasivos == null || pasivos < 0){
+                mostrarError("El valor no puede ser negativo")
+                return@setOnClickListener
+            }
             val capital = "C$ %.2f".format(calcularCapital(activos, pasivos))
             binding.inclCapitalContable.tvCapitalContable.text = capital
         }
 
         binding.inclNotaFinal.btnCalcularPromedio.setOnClickListener() {
-            val corteI = binding.inclNotaFinal.etNotaCorteI.text.toString().toInt()
-            val corteII = binding.inclNotaFinal.etNotaCorteII.text.toString().toInt()
-            val corteIII = binding.inclNotaFinal.etNotaCorteIII.text.toString().toInt()
+            val corteI = binding.inclNotaFinal.etNotaCorteI.text.toString().toIntOrNull()
+            val corteII = binding.inclNotaFinal.etNotaCorteII.text.toString().toIntOrNull()
+            val corteIII = binding.inclNotaFinal.etNotaCorteIII.text.toString().toIntOrNull()
+            if (corteI == null || corteI !in 0..100 ||
+                corteII == null || corteII !in 0..100 ||
+                corteIII == null || corteIII !in 0..100) {
+                mostrarError("Las notas deben estar entre 0 y 100.")
+                return@setOnClickListener
+            }
             val promedio = calcularPromedio(corteI, corteII, corteIII).toString()
             binding.inclNotaFinal.tvPromedio.text = promedio
         }
 
         binding.inclIvaProducto.btnCalcularIVA.setOnClickListener() {
-            val price = binding.inclIvaProducto.etPrecioProducto.text.toString().toDouble()
+            val price = binding.inclIvaProducto.etPrecioProducto.text.toString().toDoubleOrNull()
+            if (price == null || price < 0){
+                mostrarError("El valor no puede ser negativo")
+                return@setOnClickListener
+            }
             val iva = "IVA: C$ %.2f".format(calcularIVA(price))
             val priceWithIVA = "Precio con IVA aplicado: C$ %.2f".format(calcularPrecioConIVA(price, calcularIVA(price)))
             binding.inclIvaProducto.tvResultadoIVA.text = iva
@@ -41,7 +56,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.inclInssEmpleado.btnCalcularINSS.setOnClickListener() {
-            val salary = binding.inclInssEmpleado.etSalario.text.toString().toDouble()
+            val salary = binding.inclInssEmpleado.etSalario.text.toString().toDoubleOrNull()
+            if (salary == null || salary < 0){
+                mostrarError("El valor no puede ser negativo")
+                return@setOnClickListener
+            }
             val inss = "C$ %.2f".format(calcularINSS(salary))
             binding.inclInssEmpleado.tvResultadoINSS.text = inss
         }
@@ -54,7 +73,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calcularCantidadLetras(phrase : String) : Int {
-        return phrase.replace(" ", "").length
+        return phrase.count {it.isLetter()}
     }
 
     private fun calcularINSS(salary : Double) : Double {
@@ -75,5 +94,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun calcularCapital(activos: Double, pasivos: Double): Double {
         return activos - pasivos
+    }
+
+    private fun mostrarError(mensaje: String) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
     }
 }
