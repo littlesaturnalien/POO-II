6d1f12beb585a95218f644cf39a2cd3ce53ff8f2
littlesaturnalien <kmryfonseca@uamv.edu.ni>
2025-05-05 20:55:57 -0600

    Curso Kotlin Unidad 2 finalizado

---
+++ S7/Unidad 2/Kotlin/Clases_Objetos_Kotlin/src/main/kotlin/SmartDevice.kt
@@ -0,0 +1,19 @@
+open class SmartDevice(val name: String, val category: String) {
+
+    var deviceStatus = "online"
+        protected set
+
+    open val deviceType = "unknown"
+
+    open fun turnOn() {
+        deviceStatus = "on"
+    }
+
+    open fun turnOff() {
+        deviceStatus = "off"
+    }
+
+    open fun printDeviceInfo() {
+        println("Device name: $name, category: $category, type: $deviceType")
+    }
+}
